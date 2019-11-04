33296ff35ae1412819c3291fc806a995513a60de
chengyu0830 <481058422@qq.com>
2019-11-04 19:32:48 +0800

    评论列表管理

--- admin/js/comment_list.js
+++ admin/js/comment_list.js
@@ -10,3 +10,41 @@ $.ajax({
         $('#commentBox').html(html)
     }   
 })
+// 评论删除功能
+$('#commentBox').on('click', '#delete', function () {
+    var id = $(this).attr('data-id');
+    console.log(id);
+    if (confirm('确定残忍删除?')) {
+      $.ajax({
+        url: 'http://47.111.184.55:8888/api/v1/admin/comment/delete',
+        type: 'post',
+        data:{id:id},
+        success: function (res) {
+          console.log(res);
+          console.log('.....');
+          location.reload();
+        }
+      })
+    }
+
+  })
+
+
+  //审核拒绝
+  $('#commentBox').on('click', '#refuse', function () {
+    var id = $(this).attr('data-id');
+    console.log(id);
+    if(confirm('是否确定更改状态')){
+        $.ajax({
+            url:'http://47.111.184.55:8888/api/v1/admin/comment/reject',
+            type:'post',
+            data:{id:id},
+            success:function(res){
+                console.log(res);
+                // $(this).html('通过')
+               location.reload()
+            }
+        })
+    }    
+
+})
